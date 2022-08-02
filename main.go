2f09eadb7ed87c31a7c6f459484843690a8e3496
thitiphongD <thitiphong.srisavat@gmail.com>
2022-08-02 11:43:05 +0700

    test model binding and validation

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Register struct {
+	User string `json:"user" binding:"required"`
+	Pass string `json:"pass" binding:"required"`
+	Name string `json:"name" binding:"required"`
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
+
+	router.POST("/register", func(c *gin.Context) {
+		var json Register
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"register": json,
 		})
 	})
+
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
